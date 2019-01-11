f6c1ec3b16c67e3d34924ee034843cd3a82827d3
Ebru Alici <ebrugulnaz@gmail.com>
2019-01-11 16:41:31 -0700

    Add Scanner for user input.
    Expect to pass this to other methods.

--- src/com/skilldistillery/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -1,11 +1,13 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//use methods
-
+		Scanner kb = new Scanner(System.in);
 	}
 
 }
